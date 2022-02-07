41f396a81688adc7915070931214608d7e5268ed
coneagoe <coneagoe@gmail.com>
2022-02-07 20:57:53 +0800

    add tiantian fund

--- baidu_ocr_ut.py
+++ baidu_ocr_ut.py
@@ -3,8 +3,8 @@ import baidu_ocr as ocr
 
 
 class MyTestCase(unittest.TestCase):
-    def test_ocr(self):
-        words = ocr.get_ocr('test.jpg')
+    def test_position_ttjj_app(self):
+        words = ocr.get_funds_position_ttjj_app('test.jpg')
         print(words)
 
 if __name__ == '__main__':
