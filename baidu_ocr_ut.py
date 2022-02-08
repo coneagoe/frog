9762fabebd5fe4a5a48c5526c67b0f8beccf3e97
coneagoe <coneagoe@gmail.com>
2022-02-08 19:54:51 +0800

    strip whitespace in ocr result

--- baidu_ocr_ut.py
+++ baidu_ocr_ut.py
@@ -4,7 +4,7 @@ import baidu_ocr as ocr
 
 class MyTestCase(unittest.TestCase):
     def test_position_ttjj_app(self):
-        words = ocr.get_funds_position_ttjj_app('test.jpg')
+        words = ocr.get_funds_position_ttjj_app('test.jpg', ('002943', '001718', '000297', '001532', '000991'))
         print(words)
 
 if __name__ == '__main__':
