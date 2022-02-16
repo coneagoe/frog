73a0ef5c9bd33acbba65e29a66738f0b5813a008
Coneagoe <coneagoe@gmail.com>
2022-02-16 13:24:50 +0800

    feat) crawl history netvalue asynchronously refact) crop image with pillow

---
+++ fund/common.py
@@ -0,0 +1,17 @@
+# -*- coding: utf-8 -*-
+
+# path
+fund_data_path = 'data/fund'
+history_netvalue_path = 'data/fund/history_netvalue'
+all_general_info_csv = 'data/fund/all_general_info.csv'
+
+# column name
+col_fund_id = '基金代号'
+col_fund_name = '基金名'
+col_asset = '资产'
+col_yesterday_earning = '昨日收益'
+col_position_income = '持仓收益'
+col_position_yield = '持仓收益率(%)'
+col_pinyin = '拼音'
+col_pinyin_abbreviation = '拼音缩写'
+col_fund_type = '基金类型'
---
+++ fund/test1.py
@@ -0,0 +1,6 @@
+# -*- coding: utf-8 -*-
+
+import tiantian_crawler as ttc
+
+df = ttc.get_all_fund_general_info()
+print(type(df))
