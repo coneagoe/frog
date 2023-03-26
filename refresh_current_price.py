87492a1d05ac0c4b406b21dd394336dbe14bb78e
coneagoe <coneagoe@gmail.com>
2023-03-26 18:58:59 +0800

    feat) show target price and cost

--- refresh_current_price.py
+++ refresh_current_price.py
@@ -6,8 +6,6 @@ from stock import *
 
 
 sheet_names = [u"交易计划(1d)", u"持仓"]
-col_buy_count = u"买入数量"
-col_buying_price = u"买入价格"
 output_book_name = "tmp.xlsx"
 
 
@@ -16,6 +14,7 @@ conf.config = conf.parse_config()
 trading_book_path = get_trading_book_path()
 
 excel_writer = pd.ExcelWriter(output_book_name, engine='xlsxwriter')
+
 
 for sheet_name in sheet_names:
     df = pd.read_excel(trading_book_path, sheet_name=sheet_name, dtype={col_stock_id: str})
