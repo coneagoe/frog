0c84bd3063a754a88c8969064f632755808530a1
coneagoe <coneagoe@gmail.com>
2023-05-13 19:56:00 +0800

    feat) calculate ma20

--- update_trading_book.py
+++ update_trading_book.py
@@ -7,6 +7,9 @@ import swifter
 from stock import *
 
 
+col_ma_20 = u"20日均线"
+
+
 conf.config = conf.parse_config()
 
 trading_book_path = get_trading_book_path()
@@ -27,7 +30,6 @@ def update_support_resistance(df: pd.DataFrame):
 
     filter_na = df[col_buy_count].isna()
     df.loc[filter_na, col_buying_price] = df.loc[filter_na, col_current_price]
-
     df[[col_support, col_resistance]] = \
         df.swifter.apply(calculate_support_resistance, args=(start_date_ts, end_date_ts),
                          axis=1, result_type='expand')
@@ -35,9 +37,33 @@ def update_support_resistance(df: pd.DataFrame):
     return df
 
 
-for sheet_name in sheet_names:
-    df = pd.read_excel(trading_book_path, sheet_name=sheet_name, dtype={col_stock_id: str})
-    df = update_support_resistance(df)
-    df.to_excel(excel_writer, sheet_name=sheet_name)
+def calculate_ma_20(df: pd.DataFrame):
+    n = 40
+    end_date = date.today()
+    start_date = end_date - pd.Timedelta(days=n)
+    start_date_ts = start_date.strftime('%Y%m%d')
+    end_date_ts = end_date.strftime('%Y%m%d')
+    df_history_data = load_history_data(df[col_stock_id], start_date_ts, end_date_ts)
+    if df_history_data is not None:
+        ma_20 = df_history_data[col_close].rolling(window=20).mean()
+        return ma_20.iloc[-1]
+
+    return np.nan
+
+
+def update_stoploss(df: pd.DataFrame):
+    df[col_current_price] = df[col_stock_id].swifter.apply(fetch_close_price)
+    df[col_ma_20] = df.swifter.apply(calculate_ma_20, axis=1)
+
+    return df
+
+
+df = pd.read_excel(trading_book_path, sheet_name=u"交易计划(1d)", dtype={col_stock_id: str})
+df = update_support_resistance(df)
+df.to_excel(excel_writer, sheet_name=u"交易计划(1d)")
+
+df = pd.read_excel(trading_book_path, sheet_name=u"持仓", dtype={col_stock_id: str})
+df = update_stoploss(df)
+df.to_excel(excel_writer, sheet_name=u"持仓")
 
 excel_writer.close()
