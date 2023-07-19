0def0861f1021e3dba121c2ff65d306d9309f722
coneagoe <coneagoe@gmail.com>
2023-07-19 19:39:19 +0800

    refact) cleanup dependency

--- stock/__init__.py
+++ stock/__init__.py
@@ -1,4 +1,6 @@
-from . common import *
+from . const import *
+from . common import get_stock_data_path, get_stock_position_path, get_stock_general_info_path, \
+    get_etf_general_info_path, get_stock_history_path, get_stock_1d_path, get_trading_book_path
 from . account import *
 from . data import *
 from . support_resistance import get_support_resistance, get_turning_points, calculate_support_resistance
