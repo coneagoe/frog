858727c8cfe9d3751887f5230af2af574fc7ded3
coneagoe <coneagoe@gmail.com>
2023-08-06 19:53:10 +0800

    refact) cleanup monitor_fallback_stocks

--- monitor_fallback_stocks.py
+++ monitor_fallback_stocks.py
@@ -2,48 +2,27 @@
 import os.path
 import sys
 import time
-from datetime import date, datetime
+from datetime import date
 import pandas as pd
 import swifter
 import pandas_market_calendars as mcal
 import conf
-from stock import col_stock_id, col_current_price, fetch_close_price
+from stock import col_stock_id, col_current_price, fetch_close_price, is_market_open
 from utility import send_email
 
 
-col_reported = u'上报'
+col_email = 'email'
 col_monitor_price = u'监控价格'
+sleep_interval = 300
+stock_csv = 'fallback_stocks.csv'
 
 
 conf.config = conf.parse_config()
 
 market_calendar = mcal.get_calendar('XSHG')
-sleep_interval = 300
-
-
-def is_market_open():
-    if date.today().weekday() >= 5:
-        return False
-
-    now = datetime.now()
-    if (now.hour == 9 and now.minute >= 30) or (now.hour == 10) or (now.hour == 11 and now.minute <= 30) or \
-            (now.hour == 13) or (now.hour == 14) or (now.hour == 15 and now.minute <= 30):
-        return True
-
-    return False
-
-
-def usage():
-    print(f"{os.path.basename(__file__)} <stock.csv>")
 
 
 if __name__ == '__main__':
-    if len(sys.argv) < 2:
-        usage()
-        exit()
-
-    stock_csv = sys.argv[1]
-
     while True:
         if not is_market_open():
             time.sleep(sleep_interval)
@@ -53,12 +32,12 @@ if __name__ == '__main__':
         df[col_stock_id] = df[col_stock_id].astype(str)
         df[col_stock_id] = df[col_stock_id].str.zfill(6)
 
-        df0 = df[df[col_reported].isna()]
+        df0 = df[df[col_email].isna()]
         df[col_current_price] = df0[col_stock_id].swifter.apply(fetch_close_price)
 
-        df1 = df[df[col_reported].isna() & (df[col_monitor_price] >= df[col_current_price])]
-        df.loc[df1.index, col_reported] = 1
+        df1 = df[df[col_email].isna() & (df[col_monitor_price] >= df[col_current_price])]
         if not df1.empty:
+            df.loc[df1.index, col_email] = 1
             fallback_stock_output = f"fallback_stock_{date.today().strftime('%Y%m%d')}.csv"
             df1.to_csv(fallback_stock_output, encoding='GBK', index=False)
             send_email('fallback stock report', fallback_stock_output)
