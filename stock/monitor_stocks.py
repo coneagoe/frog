eabeb63bb0f334882180d3e2c9fdb274bf3d38bc
coneagoe <coneagoe@gmail.com>
2023-09-24 23:18:43 +0800

    feat) use sqlalchemy instead of raw db

--- stock/monitor_stocks.py
+++ stock/monitor_stocks.py
@@ -1,19 +1,16 @@
-from sqlalchemy import Column, Integer, String, Boolean
+from sqlalchemy import Column, Integer, String
+from . const import col_stock_id, col_stock_name, col_monitor_price
 
 
-class MonitorStock:
+class MonitorStock(db.Model):
     __tablename__ = 'monitor_stock'
 
     id = Column(Integer, primary_key=True)
-    stock_id = Column(String(6), nullable=False)
-    stock_name = Column(String(20), nullable=False)
-    current_price = Column(String(10), nullable=True)
-    monitor_price = Column(String(10), nullable=False)
-    email = Column(Boolean, nullable=False)
-    mobile = Column(Boolean, nullable=False)
-    pc = Column(Boolean, nullable=False)
-    comment = Column(String(100), nullable=False)
+    stock_id = Column(String(6), name=col_stock_id, nullable=False)
+    stock_name = Column(String(20), name=col_stock_name, nullable=False)
+    monitor_price = Column(String(10), name=col_monitor_price, nullable=False)
+    comment = Column(String(50))
 
     def __repr__(self):
-        return f"<MonitorStock(stock_id={self.stock_id}, " \"monitor_price={self.monitor_price}, " \
-               f"email={self.email}, mobile={self.mobile}, pc={self.pc}, comment={self.comment})>"
+        return f"<MonitorStock(stock_id={self.stock_id}, stock_name={self.stock_name}, " \
+               f"monitor_price={self.monitor_price}, comment={self.comment})>"
