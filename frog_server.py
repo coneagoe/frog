f3a691e52a0090409f08a4508daca51f9d5af1bc
coneagoe <coneagoe@gmail.com>
2023-12-10 13:45:57 +0800

    refact) define model class in a seperate module file

--- frog_server.py
+++ frog_server.py
@@ -2,11 +2,7 @@ import base64
 from datetime import date
 import io
 from flask import Flask, request
-from flask_sqlalchemy import SQLAlchemy
 from flask_apscheduler import APScheduler
-from sqlalchemy import Column, Integer, String
-from sqlalchemy.orm import Mapped
-from sqlalchemy.ext.declarative import declarative_base
 import pandas as pd
 
 import conf
@@ -15,6 +11,8 @@ from stock import col_stock_id, col_stock_name, col_current_price, \
     monitor_stock_table_name, fetch_close_price, get_yesterday_ma, \
     is_market_open
 from utility import send_email
+from db import db
+from strategy import MonitorStock
 
 
 col_period = 'period'
@@ -26,26 +24,6 @@ conf.parse_config()
 
 class Config(object):
     SCHEDULER_API_ENABLED = True
-
-
-Base = declarative_base()
-
-
-db = SQLAlchemy(model_class=Base)
-
-
-class MonitorStock(db.Model):
-    __tablename__ = monitor_stock_table_name
-
-    id: Mapped[int] = Column(Integer, primary_key=True)
-    stock_id: Mapped[str] = Column(String(6), name=col_stock_id, nullable=False)
-    stock_name: Mapped[str] = Column(String(20), name=col_stock_name, nullable=False)
-    monitor_price: Mapped[str] = Column(String(10), name=col_monitor_price, nullable=False)
-    comment: Mapped[str] = Column(String)
-
-    def __repr__(self):
-        return f"<MonitorStock(stock_id={self.stock_id}, stock_name={self.stock_name}, " \
-               f"monitor_price={self.monitor_price}, comment={self.comment})>"
 
 
 app = Flask(__name__)
