a93d0d0d090bf325739c668cde835957c2c21c82
coneagoe <coneagoe@gmail.com>
2024-01-01 13:58:41 +0800

    feat) support NASDAQ, DJI and S&P 500 comparision

--- compare_two_securities.py
+++ compare_two_securities.py
@@ -5,7 +5,7 @@ import pandas as pd
 import plotly.graph_objs as go
 import conf
 from stock import (
-    get_stock_or_etf_name,
+    get_security_name,
     col_date,
     col_close,
     load_history_data
@@ -16,8 +16,8 @@ conf.parse_config()
 
 
 def show_two_securities(stock_id_0: str, df0: pd.DataFrame, stock_id_1: str, df1: pd.DataFrame):
-    stock_name_0 = get_stock_or_etf_name(stock_id_0)
-    stock_name_1 = get_stock_or_etf_name(stock_id_1)
+    stock_name_0 = get_security_name(stock_id_0)
+    stock_name_1 = get_security_name(stock_id_1)
 
     fig = go.Figure()
 
@@ -29,29 +29,21 @@ def show_two_securities(stock_id_0: str, df0: pd.DataFrame, stock_id_1: str, df1
     fig.show()
 
 
-def usage():
-    print(f"{basename(__file__)} [-n days] [-s start_date] [-e end_date] <stock_id_0> <stock_id_1>")
-    print("start_date/end_date: YYYYMMDD")
-
-
 if __name__ == "__main__":
     parser = argparse.ArgumentParser()
-    parser.add_argument('-n', default=180, type=int,
+    parser.add_argument('-n', default=360, type=int,
                         help='since how many days ago to compare, cannot be used together with -s and -e')
     parser.add_argument('-s', type=str, help='start date, YYYYMMDD')
     parser.add_argument('-e', type=str, help='end date, YYYYMMDD')
-    parser.add_argument('start_id_0', type=str)
-    parser.add_argument('start_id_1', type=str)
+    parser.add_argument('security_id_0', type=str,
+                        help='can be .IXIC(NASDAQ Composite), .DJI(Dow Jones Industrial Average) and .INX(S&P 500)')
+    parser.add_argument('security_id_1', type=str)
     args = parser.parse_args()
 
-    stock_id_0 = args.start_id_0
-    stock_id_1 = args.start_id_1
+    security_id_0 = args.security_id_0
+    security_id_1 = args.security_id_1
     start_date = args.s
     end_date = args.e
-
-    if stock_id_0 is None or stock_id_1 is None:
-        usage()
-        exit()
 
     if start_date is None or end_date is None:
         end_date = date.today()
@@ -59,7 +51,7 @@ if __name__ == "__main__":
         start_date = start_date.strftime('%Y%m%d')
         end_date = end_date.strftime('%Y%m%d')
 
-    df0 = load_history_data(stock_id_0, start_date, end_date)
-    df1 = load_history_data(stock_id_1, start_date, end_date)
+    df0 = load_history_data(security_id_0, start_date, end_date)
+    df1 = load_history_data(security_id_1, start_date, end_date)
 
-    show_two_securities(stock_id_0, df0, stock_id_1, df1)
+    show_two_securities(security_id_0, df0, security_id_1, df1)
--- strategy/__init__.py
+++
@@ -1,1 +0,0 @@
-from .monitor_stack import MonitorStock
