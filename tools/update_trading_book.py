57a467a5d56176144feefb3415060e11c0fccb21
coneagoe <coneagoe@gmail.com>
2024-02-19 15:00:50 +0800

    refact) update support and resistance in etf

--- tools/update_trading_book.py
+++ tools/update_trading_book.py
@@ -58,6 +58,7 @@ def update_current_price(df: pd.DataFrame):
 
 df = pd.read_excel(trading_book_path, sheet_name=sheet_name_etf, dtype={COL_STOCK_ID: str})
 df = update_current_price(df)
+df = update_support_resistance(df)
 df.to_excel(excel_writer, sheet_name=sheet_name_etf)
 
 df = pd.read_excel(trading_book_path, sheet_name=sheet_name_stock, dtype={COL_STOCK_ID: str})
