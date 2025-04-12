c16c5c7c25499ce0a078fda779df80891cf3165f
coneagoe <coneagoe@gmail.com>
2025-04-12 19:37:48 +0800

    refact) send_email

--- utility/__init__.py
+++ utility/__init__.py
@@ -1,2 +1,20 @@
-from .utility import *
-from .send_email import send_email
+from . utility import (
+    is_older_than_n_days,
+    is_older_than_a_week,
+    is_older_than_a_month,
+    is_older_than_a_quarter,
+    retry_async,
+    retry_sync,
+)
+from . send_email import send_email
+
+
+__all__ = [
+    "is_older_than_n_days",
+    "is_older_than_a_week",
+    "is_older_than_a_month",
+    "is_older_than_a_quarter",
+    "retry_async",
+    "retry_sync",
+    "send_email",
+]
--- utility/send_email.py
+++ utility/send_email.py
@@ -1,3 +1,4 @@
+import logging
 import os
 import smtplib
 from email.mime.text import MIMEText
@@ -5,20 +6,32 @@ from email.mime.multipart import MIMEMultipart
 from email.mime.application import MIMEApplication
 
 
-def send_email(subject: str, attachment_file_name: str):
+def send_email(subject: str, body: str, attachment_file_name: str | None = None):
+    mail_server = os.getenv("MAIL_SERVER")
+    mail_port = os.getenv("MAIL_PORT")
+    mail_sender = os.getenv("MAIL_SENDER")
+    mail_password = os.getenv("MAIL_PASSWORD")
+    mail_receivers = os.getenv("MAIL_RECEIVERS")
+
+    if not all([mail_server, mail_port, mail_sender,
+                mail_password, mail_receivers]):
+        logging.error("Email configuration is missing.")
+        return
+
     message = MIMEMultipart()
-    message['From'] = os.environ['email_sender']
-    message['To'] = os.environ['email_receiver']
+    message['From'] = mail_sender
+    message['To'] = mail_receivers
     message['Subject'] = subject
 
-    body = 'Please find the attachment.'
     message.attach(MIMEText(body, 'plain'))
 
-    with open(attachment_file_name, 'rb') as attachment:
-        part = MIMEApplication(attachment.read(), Name=attachment_file_name)
-        part['Content-Disposition'] = f'attachment; filename="{attachment_file_name}"'
-        message.attach(part)
+    if attachment_file_name is not None:
+        with open(attachment_file_name, 'rb') as attachment:
+            part = MIMEApplication(attachment.read(), Name=attachment_file_name)
+            part['Content-Disposition'] = f'attachment; filename="{attachment_file_name}"'
+            message.attach(part)
 
-    with smtplib.SMTP_SSL(os.environ['email_server'], os.environ['email_server_port']) as smtp:
-        smtp.login(os.environ['email_user'], os.environ['email_password'])
+    with smtplib.SMTP_SSL(mail_server, mail_port) as smtp:
+        smtp.login(mail_sender, mail_password)
         smtp.send_message(message)
+        smtp.quit()
