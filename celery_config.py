1d14492299cae3bc9cb9044566c02928d077261d
coneagoe <coneagoe@gmail.com>
2025-07-05 19:35:30 +0800

    feat) add obos_hk task

--- celery_config.py
+++ celery_config.py
@@ -5,9 +5,13 @@ result_backend = 'redis://localhost:6379/0'
 
 # 定义定时任务
 beat_schedule = {
-    'run-backtest-daily': {
-        'task': 'task.trend_follow_etf.trend_follow_etf',
-        'schedule': crontab(hour=15, minute=5),
+    # 'run-backtest-daily': {
+    #     'task': 'task.trend_follow_etf.trend_follow_etf',
+    #     'schedule': crontab(hour=15, minute=5),
+    # },
+    'download-hk-stock-data': {
+        'task': 'task.download_hk_stock_data.download_hk_stock_data',
+        'schedule': crontab(hour=15, minute=0, day_of_week='mon-fri'),
     },
 
 #     'run-monitor-hourly': {
--- task/__init__.py
+++ task/__init__.py
@@ -1,8 +1,10 @@
 from .trend_follow_etf import trend_follow_etf
 from .obos_hk import obos_hk
+from .download_hk_stock_data import download_hk_stock_data
 
 
 __all__ = [
     'trend_follow_etf',
     'obos_hk',
+    'download_hk_stock_data',
 ]
