47dff4300867a011b7507e9705fd85e07a367136
coneagoe <coneagoe@gmail.com>
2025-07-15 21:08:40 +0800

    feat) run frog in docker

--- start_celery.sh
+++ start_celery.sh
@@ -1,10 +1,5 @@
 #!/bin/bash
 
-# 激活 Python 虚拟环境
-source env/bin/activate
-
-# 以后台方式启动 Celery worker
 celery -A celery_app worker --loglevel=info &
 
-# 启动 Celery beat
 celery -A celery_app beat --loglevel=info
