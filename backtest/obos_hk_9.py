2bf8db40bbdbe0bce1f34b38c0b2de4340b4f545
coneagoe <coneagoe@gmail.com>
2025-08-09 20:05:50 +0800

    feat) support 0.5% of total capital per trade

---
+++ backtest/obos_hk_9.py
@@ -0,0 +1,163 @@
+import argparse
+import os
+import sys
+
+import backtrader as bt
+import pandas as pd
+
+sys.path.append(os.path.join(os.path.dirname(__file__), ".."))
+from common import drop_suspended, run  # noqa: E402
+from my_strategy import MyStrategy, OrderState  # noqa: E402
+from obos_indicator import OBOS  # noqa: E402
+from stop_price_manager import StopPriceManagerEma as StopPriceManager  # noqa: E402
+
+import conf  # noqa: E402
+from indicator import (  # noqa: E402
+    OBOS_OVERBUY_THRESHOLD,
+    OBOS_OVERSELL_THRESHOLD,
+    OBOS_PARAM_M,
+    OBOS_PARAM_N,
+)
+from stock import drop_delisted_stocks  # noqa: E402
+from stock import COL_STOCK_ID, load_all_hk_ggt_stock_general_info  # noqa: E402
+
+conf.parse_config()
+
+
+class ObosStrategy(MyStrategy):
+    params = (
+        ("param_n", OBOS_PARAM_N),
+        ("param_m", OBOS_PARAM_M),
+        ("param_sp", 5),  # 过去n天的最低价作为initial stop price
+        ("target", 0.005),  # 单笔仓位占比(例如 0.005 表示 0.5%)
+    )
+
+    def __init__(self):
+        super().__init__()
+
+        self.obos = {
+            i: OBOS(self.datas[i], n=self.p.param_n, m=self.p.param_m)
+            for i in range(len(self.datas))
+        }
+
+        self.kdj = {
+            i: bt.indicators.Stochastic(
+                self.datas[i],
+                period=14,  # 增加到14天，减少敏感度
+                period_dfast=3,  # K值的平滑周期
+                period_dslow=3,  # D值的平滑周期
+                safediv=True,  # 启用安全除法，避免除零错误
+            )
+            for i in range(len(self.datas))
+        }
+
+        self.stop_manager = StopPriceManager(self.datas, 0.2)
+
+    def is_over_sell(self, i):
+        try:
+            j_value = 3 * self.kdj[i].percK[0] - 2 * self.kdj[i].percD[0]
+            kdj_oversell = j_value < -10
+        except (ZeroDivisionError, IndexError):
+            kdj_oversell = False
+
+        return (self.obos[i] < OBOS_OVERSELL_THRESHOLD) or kdj_oversell
+
+    def next(self):
+        super().next()
+
+        for i in range(len(self.datas)):
+            if self.context[i].order_state == OrderState.ORDER_IDLE:
+                if self.context[i].is_candidator is False:
+                    if self.is_over_sell(i):
+                        self.context[i].is_candidator = True
+                        continue
+                else:
+                    # 如果close > ema20
+                    if self.context[i].current_price > self.stop_manager.ema20[i][0]:
+                        self.order_target_percent(self.datas[i], target=self.p.target)
+                        self.context[i].stop_price = min(
+                            [
+                                self.datas[i].low[-j]
+                                for j in range(1, self.p.param_sp + 1)
+                            ]
+                        )
+                        self.context[i].order_state = OrderState.ORDER_PRE_OPENING
+            elif self.context[i].order_state == OrderState.ORDER_HOLDING:
+                self.stop_manager.update_stop_price(self.context, self.datas, i)
+                # 如果OBOS超买
+                if self.obos[i] > OBOS_OVERBUY_THRESHOLD:
+                    self.context[i].stop_price = max(
+                        self.context[i].stop_price, self.datas[i].low[-1]
+                    )
+
+                if self.context[i].current_price < self.context[i].stop_price:
+                    self.order_target_percent(self.datas[i], target=0.0)
+                    self.context[i].order_state = OrderState.ORDER_CLOSING
+
+    def notify_trade(self, trade):
+        super().notify_trade(trade)
+
+        if trade.isopen:
+            i = self.stocks.index(trade.getdataname())
+            self.context[i].stop_price = min(
+                [self.datas[i].low[-j] for j in range(1, self.p.param_sp + 1)]
+            )
+
+    # def stop(self):
+    #     print('ema_period: %d, n_positions: %d' %
+    #         (self.p.ema_period, self.p.n_portion))
+    #     super().stop()
+
+
+if __name__ == "__main__":
+    parser = argparse.ArgumentParser()
+    parser.add_argument(
+        "-s", "--start", required=True, help="Start date in YYYY-MM-DD format"
+    )
+    parser.add_argument(
+        "-e", "--end", required=True, help="End date in YYYY-MM-DD format"
+    )
+    parser.add_argument(
+        "-f",
+        "--filter",
+        required=False,
+        help="Space-separated list of stock IDs to filter out",
+    )
+    parser.add_argument(
+        "-c", "--cash", required=False, type=float, help="Initial cash amount"
+    )
+    parser.add_argument("-p", "--plot", required=False, default="", help="Plot trade")
+    args = parser.parse_args()
+
+    os.environ["PLOT_TRADE"] = args.plot
+    if args.cash:
+        os.environ["INIT_CASH"] = str(args.cash)
+
+    hk_ggt_stocks_df = load_all_hk_ggt_stock_general_info()
+
+    stocks = hk_ggt_stocks_df[COL_STOCK_ID].tolist()
+    if args.filter:
+        filter_list = args.filter.split()
+        stocks = [stock for stock in stocks if stock not in filter_list]
+
+    stocks = drop_delisted_stocks(stocks, args.start, args.end)
+    ObosStrategy.stocks = drop_suspended(stocks, args.start, args.end, 10)
+
+    cerebro = bt.Cerebro()
+
+    if os.environ.get("OPTIMIZER") == "True":
+        strats = cerebro.optstrategy(ObosStrategy, param_sp=range(1, 10))
+    else:
+        cerebro.addstrategy(ObosStrategy)
+
+    df_stocks = pd.DataFrame(ObosStrategy.stocks, columns=[COL_STOCK_ID])
+
+    strategy_name = os.path.splitext(os.path.basename(__file__))[0]
+    run(
+        strategy_name=strategy_name,
+        cerebro=cerebro,
+        stocks=ObosStrategy.stocks,
+        start_date=args.start,
+        end_date=args.end,
+        security_type="hk_ggt_stock",
+    )
