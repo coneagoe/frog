eb8c69064bf6ea5f522e6eda10a4d11a88f030da
coneagoe <coneagoe@gmail.com>
2025-09-06 22:49:22 +0800

    refact) [stock] use enum instead of str

--- stock/data/downloaders/__init__.py
+++ stock/data/downloaders/__init__.py
@@ -1,7 +1,16 @@
+from enum import Enum
 from .base import DataDownloader
 from .akshare_downloader import AkshareDownloader
 
+
+class AdjustType(Enum):
+    BFQ = '',  # 不复权 (no adjustment)
+    QFQ = 'qfq',  # 前复权 (forward adjustment)
+    HFQ = 'hfq'   # 后复权 (backward adjustment)
+
+
 __all__ = [
     'DataDownloader',
-    'AkshareDownloader'
+    'AkshareDownloader',
+    'PriceType'
 ]
