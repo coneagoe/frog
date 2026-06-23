6210cf41062eaf787f1e9e19ba73fdba0d8b5520
coneagoe <coneagoe@gmail.com>
2026-06-23 15:05:21 +0800

    chore: remove stock/account module and guotai parse tool

--- stock/account/__init__.py
+++
@@ -1,8 +0,0 @@
-from .guotai import GuotaiParser
-from .history_position import load_history_position, load_history_positions
-
-__all__ = [
-    "load_history_position",
-    "load_history_positions",
-    "GuotaiParser",
-]
