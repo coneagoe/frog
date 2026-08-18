3049cad21328f53db32960a13df3a4236cbb4379
coneagoe <coneagoe@gmail.com>
2026-08-18 14:36:51 +0800

    feat: add core index diagnostic metadata

--- download/core_indexes.py
+++ download/core_indexes.py
@@ -27,3 +27,18 @@ CORE_INDEX_TS_CODES = MappingProxyType(
         CoreIndexGroup.SZSE_100: "399330.SZ",
     }
 )
+
+
+CORE_INDEX_DISPLAY_NAMES = MappingProxyType(
+    {
+        CoreIndexGroup.CSI_300: "沪深300",
+        CoreIndexGroup.SSE_50: "上证50",
+        CoreIndexGroup.SSE_180: "上证180",
+        CoreIndexGroup.CSI_500: "中证500",
+        CoreIndexGroup.CSI_800: "中证800",
+        CoreIndexGroup.CSI_1000: "中证1000",
+        CoreIndexGroup.CHINEXT: "创业板指",
+        CoreIndexGroup.STAR_50: "科创50",
+        CoreIndexGroup.SZSE_100: "深证100",
+    }
+)
--- test/download/test_core_indexes.py
+++ test/download/test_core_indexes.py
@@ -13,3 +13,25 @@ def test_core_index_mapping_pins_broad_market_codes():
         CoreIndexGroup.STAR_50: "000688.SH",
         CoreIndexGroup.SZSE_100: "399330.SZ",
     }
+
+
+def test_core_index_display_names_cover_pinned_groups():
+    from download.core_indexes import CORE_INDEX_DISPLAY_NAMES
+
+    assert CORE_INDEX_DISPLAY_NAMES == {
+        CoreIndexGroup.CSI_300: "沪深300",
+        CoreIndexGroup.SSE_50: "上证50",
+        CoreIndexGroup.SSE_180: "上证180",
+        CoreIndexGroup.CSI_500: "中证500",
+        CoreIndexGroup.CSI_800: "中证800",
+        CoreIndexGroup.CSI_1000: "中证1000",
+        CoreIndexGroup.CHINEXT: "创业板指",
+        CoreIndexGroup.STAR_50: "科创50",
+        CoreIndexGroup.SZSE_100: "深证100",
+    }
+
+
+def test_core_index_display_names_match_turnover_mapping_keys():
+    from download.core_indexes import CORE_INDEX_DISPLAY_NAMES
+
+    assert set(CORE_INDEX_DISPLAY_NAMES) == set(CORE_INDEX_TS_CODES)
